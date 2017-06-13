69a388ba435ecf4b8ad305c2c68cbeabeebf05a5
sang84020325 <879689064@qq.com>
2017-06-14 00:26:16 +0800

    兼容rxjava2

--- app/src/main/java/com/basemoudle/sample/model/MVPModel.java
+++ app/src/main/java/com/basemoudle/sample/model/MVPModel.java
@@ -5,9 +5,7 @@ import com.basemoudle.sample.bean.User;
 import com.basemoudle.sample.contract.MVPContract;
 import com.xusangbo.basemoudle.base.BaseRespose;
 import com.xusangbo.basemoudle.baserx.RxSchedulers;
-
 import io.reactivex.Flowable;
-import io.reactivex.FlowableTransformer;
 
 
 /**
